69e3e125e55aedbfaece5aa440bbe37cf1f7ea2a
Marek <marekgacek45@gmail.com>
2024-02-13 12:37:12 +0100

    update project

--- src/js/main.js
+++ src/js/main.js
@@ -147,6 +147,20 @@ if (filterItem && filterImages.length > 0) {
 
 
 
+function addLanguageScript(lang) {
+    const scriptUrl = lang === 'pl'
+      ? 'https://wis.upperbooking.com/owcedwie/be-panel?locale=pl'
+      : 'https://wis.upperbooking.com/owcedwie/be-panel?locale=en';
+  
+    const scriptElement = document.createElement('script');
+    scriptElement.src = scriptUrl;
+    scriptElement.async = true;
+  
+    document.body.appendChild(scriptElement);
+
+  
+
+  }
  // Function to fetch language data
  async function fetchLanguageData(lang) {
     const response = await fetch(`languages/${lang}.json`);
@@ -168,6 +182,35 @@ function updateContent(langData, lang) {
   
     // Update the 'lang' attribute of the <html> element
     document.documentElement.lang = lang;
+
+    addLanguageScript(lang);
+
+    const nameElement = document.querySelector('#Name');
+    if (nameElement) {
+        nameElement.placeholder = langData['name_placeholder'];
+    }
+
+    const phoneElement = document.querySelector('#phone');
+    if (phoneElement) {
+        phoneElement.placeholder = langData['phone_placeholder'];
+    }
+
+    const emailElement = document.querySelector('#Email');
+    if (emailElement) {
+        emailElement.placeholder = langData['email_placeholder'];
+    }
+
+    const messageElement = document.querySelector('#Message');
+    if (messageElement) {
+        messageElement.placeholder = langData['message_placeholder'];
+    }
+
+    const submitButton = document.querySelector('input[type=submit]');
+    if (submitButton) {
+        submitButton.value = langData['submit_button'];
+    }
+
+
   }
   
   // Function to change language
@@ -184,5 +227,50 @@ function updateContent(langData, lang) {
     const langData = await fetchLanguageData(userPreferredLanguage);
     updateContent(langData, userPreferredLanguage);
   });
+
+
+//  // Function to fetch language data
+//  async function fetchLanguageData(lang) {
+//     const response = await fetch(`languages/${lang}.json`);
+//     return response.json();
+//   }
+  
+//   // Function to set the language preference
+//   function setLanguagePreference(lang) {
+//     localStorage.setItem('language', lang);
+//     location.reload();
+//   }
+  
+//   // Function to update content based on selected language
+// function updateContent(langData, lang) {
+//     document.querySelectorAll('[data-i18n]').forEach(element => {
+//       const key = element.getAttribute('data-i18n');
+//       element.textContent = langData[key];
+//     });
+  
+//     // Update the 'lang' attribute of the <html> element
+//     document.documentElement.lang = lang;
+
+//     document.querySelector('#Name').placeholder = langData['name_placeholder'];
+//     document.querySelector('#phone').placeholder = langData['phone_placeholder'];
+//     document.querySelector('#Email').placeholder = langData['email_placeholder'];
+//     document.querySelector('#Message').placeholder = langData['message_placeholder'];
+//     document.querySelector('input[type=submit]').value = langData['submit_button'];
+//   }
+  
+//   // Function to change language
+//   async function changeLanguage(lang) {
+//     await setLanguagePreference(lang);
+    
+//     const langData = await fetchLanguageData(lang);
+//     updateContent(langData, lang);
+//   }
+  
+//   // Call updateContent() on page load
+//   window.addEventListener('DOMContentLoaded', async () => {
+//     const userPreferredLanguage = localStorage.getItem('language') || 'pl';
+//     const langData = await fetchLanguageData(userPreferredLanguage);
+//     updateContent(langData, userPreferredLanguage);
+//   });
 
 
